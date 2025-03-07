4f23267ef43ee17e2556423b1538d7bc7a1b6c7d
KrishnaaBhavsar <krishnabhavsar7425@gmail.com>
2025-03-07 18:20:19 +0530

    auth route completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.route.js";
 import { connectToDb } from "./lib/db.js";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser"
 const app = express();
 
 const PORT=process.env.PORT;
@@ -10,6 +11,8 @@ const PORT=process.env.PORT;
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,8 @@
 import express from "express";
 
-
-
 const router = express.Router();
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { checkAuth,signup, login, logout,updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 
 router.post("/signup",signup);
@@ -12,4 +11,12 @@ router.post("/login",login);
 
 router.post("/logout",logout);
 
+
+
+// for updating a profile pic
+router.put("/update-profile", protectRoute , updateProfile);
+
+router.get("/check",protectRoute,checkAuth);
+
 export default router;
+
