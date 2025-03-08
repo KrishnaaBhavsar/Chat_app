1ae39aab8ec742ff5addd4133c7ee0e0c3e93271
KrishnaaBhavsar <krishnabhavsar7425@gmail.com>
2025-03-08 13:30:59 +0530

    message route completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectToDb } from "./lib/db.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port", PORT);
